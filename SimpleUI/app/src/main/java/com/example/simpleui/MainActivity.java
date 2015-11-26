868f3a16ab3634c64138f060715182b6a9ff0bc2
caca0923 <caca@cacadeMacBook-Pro.local>
2015-11-26 21:30:03 +0800

    hideCheckBox

--- SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.simpleui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText inputText1;
     private EditText inputText2;
+    private CheckBox hideCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +21,13 @@ public class MainActivity extends AppCompatActivity {
         inputText1.setText("12345");
         inputText2 = (EditText)findViewById(R.id.editText);
         inputText2.setText("54321");
+
+        hideCheckBox = (CheckBox)findViewById(R.id.hideCheckBox);
+        hideCheckBox.setChecked(true);
     }
 
     public void submit(View view){
-        String text = inputText1.getText().toString();
+        String text = inputText2.getText().toString();
         Toast.makeText(this,text,Toast.LENGTH_LONG).show();
     }
 }
