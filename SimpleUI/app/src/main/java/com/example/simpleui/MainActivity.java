b82b49ae673a3bbda141f3164261053537d6a2f0
caca0923 <caca@cacadeMacBook-Pro.local>
2015-11-30 20:43:18 +0800

    add setHistory

--- SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
@@ -6,9 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText inputText1;
     private EditText inputText2;
     private CheckBox hideCheckBox;
+    private ListView historyListView;
 
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
@@ -50,13 +53,21 @@ public class MainActivity extends AppCompatActivity {
         hideCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                editor.putBoolean("hideCheckBox",isChecked);
+                editor.putBoolean("hideCheckBox", isChecked);
                 editor.commit();
             }
         });
         hideCheckBox.setChecked(sharedPreferences.getBoolean("hideCheckBox",false));
         //hideCheckBox.setChecked(true);
 
+        historyListView = (ListView)findViewById(R.id.historyListView);
+        setHistory();
+    }
+
+    private void setHistory(){
+        String[] data = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        historyListView.setAdapter(adapter);
     }
 
     public void submit(View view){
